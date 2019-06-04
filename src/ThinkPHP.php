273be00030784c0427e71f762bef4ff20e996f35
tider <tider@quansitech.com>
2019-06-04 09:37:33 +0800

    t

--- src/ThinkPHP.php
+++ src/ThinkPHP.php
@@ -31,7 +31,7 @@ const URL_COMPAT        =   3;  // 兼容模式
 const EXT               =   '.class.php'; 
 
 // 系统常量定义
-defined('THINK_PATH')   or define('THINK_PATH', __DIR__ . 'ThinkPHP.php/');
+defined('THINK_PATH')   or define('THINK_PATH', __DIR__ . '/');
 defined('APP_PATH')     or define('APP_PATH',       dirname($_SERVER['SCRIPT_FILENAME']).'/');
 defined('APP_STATUS')   or define('APP_STATUS',     ''); // 应用状态 加载对应的配置文件
 defined('APP_DEBUG')    or define('APP_DEBUG',      false); // 是否调试模式
