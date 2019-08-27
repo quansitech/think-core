b83454a4b2959bfba5c569c5012e00c75f134d15
viki <719632790@qq.com>
2019-08-27 15:15:20 +0800

    u

---
+++ asset/libs/label-select/README.md
@@ -0,0 +1,12 @@
+# labelSelect
+labelSelect插件
+
+必须全局引入jQuery
+
+
+1.安装依赖 npm install
+2.运行webpack 打包 webpack (如需监听打包 webpack --watch)
+
+====需要在跟目录运行服务器   如果已经运行请忽略====
+3.安装http-serve:  npm i -g http-serve
+4.http-serve . -o -c-1 自动打开浏览器
---
+++ asset/libs/label-select/webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var dir = path.resolve(__dirname);
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+// const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = function() {
+    return {
+    	mode: 'production',
+    	// mode: 'development',
+        entry: {
+            'label-select': './label-select.js',
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(dir, 'dist/'),
+            libraryTarget: 'umd',
+        },
+        module: {  
+	        rules: [  
+	            {  
+	                test: /\.less$/,  
+	                use: ['style-loader','css-loader','less-loader']
+	            },
+                {  
+                    test: /\.css$/,  
+                    use: ['style-loader','css-loader']
+                } 
+	        ]  
+	    },
+        externals: [
+            'jquery'
+        ]
+    };
+}
