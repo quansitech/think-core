1a127c11402b3cd6c77163e4cfbd9aaab48f3c66
jack <958691165@qq.com>
2019-09-18 11:27:02 +0800

    封装BaseBuilder

---
+++ src/Library/Qscmf/Builder/BaseBuilder.class.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Qscmf\Builder;
+
+use Qscmf\Lib\DBCont;
+use Think\Controller;
+
+class BaseBuilder extends Controller
+{
+    private $_nid;
+    private $_meta_title;
+    /**
+     * @var 模版
+     */
+    protected $_template;
+    /**
+     * @var 额外功能代码
+     */
+    private $_extra_html;
+    /**
+     * @var array
+     */
+    private $_tab_nav=[];
+
+    public function setNIDByNode($module, $controller, $action){
+        $module_ent = D('Node')->where(['name' => $module, 'level' => DBCont::LEVEL_MODULE, 'status' => DBCont::NORMAL_STATUS])->find();
+
+        if(!$module_ent){
+            E('setNIDByNode 传递的参数module不存在');
+        }
+
+        $controller_ent = D('Node')->where(['name' => $controller, 'level' => DBCont::LEVEL_CONTROLLER, 'status' => DBCont::NORMAL_STATUS, 'pid' => $module_ent['id']])->find();
+        if(!$controller_ent){
+            E('setNIDByNode 传递的参数controller不存在');
+        }
+
+        $action_ent = D('Node')->where(['name' => $action, 'level' => DBCont::LEVEL_ACTION, 'status' => DBCont::NORMAL_STATUS, 'pid' => $controller_ent['id']])->find();
+        if(!$action_ent){
+            E('setNIDByNode 传递的参数action不存在');
+        }
+        else{
+            return $this->setNID($action_ent['id']);
+        }
+    }
+
+
+    public function setNID($nid){
+        $this->_nid = $nid;
+        return $this;
+    }
+
+
+    /**
+     * 设置页面标题
+     * @param $title 标题文本
+     * @return $this
+     */
+    public function setMetaTitle($meta_title) {
+        $this->_meta_title = $meta_title;
+        return $this;
+    }
+
+
+
+
+    /**
+     * 设置Tab按钮列表
+     * @param $tab_list Tab列表  array(
+    'title' => '标题',
+    'href' => 'http://www.corethink.cn'
+    )
+     * @param $current_tab 当前tab
+     * @return $this
+     */
+    public function setTabNav($tab_list, $current_tab) {
+        $this->_tab_nav = array(
+            'tab_list' => $tab_list,
+            'current_tab' => $current_tab
+        );
+        return $this;
+    }
+
+    /**
+     * 设置额外功能代码
+     * @param $extra_html 额外功能代码
+     * @return $this
+     */
+    public function setExtraHtml($extra_html) {
+        $this->_extra_html = $extra_html;
+        return $this;
+    }
+
+    /**
+     * 设置页面模版
+     * @param $template 模版
+     * @return $this
+     */
+    public function setTemplate($template) {
+        $this->_template = $template;
+        return $this;
+    }
+
+}
