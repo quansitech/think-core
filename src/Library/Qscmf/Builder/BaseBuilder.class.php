7f7c66bcfb5af9441aea3573ca51387fd7e2715d
xhiny <axhiny@163.com>
2019-10-25 10:12:38 +0800

    setNIDByNode添加默认值

--- src/Library/Qscmf/Builder/BaseBuilder.class.php
+++ src/Library/Qscmf/Builder/BaseBuilder.class.php
@@ -22,7 +22,7 @@ class BaseBuilder extends Controller
      */
     protected $_tab_nav=[];
 
-    public function setNIDByNode($module, $controller, $action){
+    public function setNIDByNode($module = MODULE_NAME, $controller = CONTROLLER_NAME, $action = 'index'){
         $module_ent = D('Node')->where(['name' => $module, 'level' => DBCont::LEVEL_MODULE, 'status' => DBCont::NORMAL_STATUS])->find();
 
         if(!$module_ent){
