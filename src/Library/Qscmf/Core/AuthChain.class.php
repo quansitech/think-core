c46329edd4cf471ab059972690e43633394f3be0
xhiny <axhiny@163.com>
2019-12-18 16:15:29 +0800

    扩展auth_ref_rule配置，可自定义不同用户类型的权限过滤

--- src/Library/Qscmf/Core/AuthChain.class.php
+++ src/Library/Qscmf/Core/AuthChain.class.php
@@ -5,7 +5,7 @@ namespace Qscmf\Core;
 class AuthChain
 {
     // 设置权限过滤标识key的session值
-    public function setAuthFilterKey($role_id, $role_type = ''){
+    public static function setAuthFilterKey($role_id, $role_type = ''){
         $role_id = $role_id ? $role_id : null;
         $role_type = ($role_id && $role_type) ? $role_type : null;
 
@@ -14,7 +14,7 @@ class AuthChain
     }
 
     // 清空权限过滤标识key的session值
-    public function cleanAuthFilterKey(){
+    public static function cleanAuthFilterKey(){
         session('AUTH_RULE_ID', null);
         session('AUTH_ROLE_TYPE', null);
     }
