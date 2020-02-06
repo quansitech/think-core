103c6105ac8c9381fcfe881f736d9dda588f39a6
tiderjian <tider@quansitech.com>
2020-02-07 01:30:31 +0800

    生成的common-cache存在绝对路径，将其转成常量+相对路径的模式

--- src/Library/Think/Think.class.php
+++ src/Library/Think/Think.class.php
@@ -89,7 +89,7 @@ class Think {
           L(include THINK_PATH.'Lang/'.strtolower(C('DEFAULT_LANG')).'.php');
 
           if(!APP_DEBUG){
-              $content  .=  "\nnamespace { Think\\Think::addMap(".var_export(self::$_map,true).");";
+              $content  .=  self::absoluteToConst("\nnamespace { Think\\Think::addMap(".var_export(self::$_map,true).");");
               $content  .=  "\nL(".var_export(L(),true).");\nC(".var_export(C(),true).');Think\Hook::import('.var_export(Hook::get(),true).');}';
               Storage::put($runtimefile,strip_whitespace('<?php '.$content));
           }else{
@@ -350,5 +350,9 @@ class Think {
                 $_trace[$level][]   =   $info;
             }
         }
+    }
+
+    static private function absoluteToConst($content){
+        return str_replace("'" . CORE_PATH, "CORE_PATH . '", $content);
     }
 }
