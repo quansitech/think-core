ca173755906b49f8c56bf7966338f75e982662c6
tiderjian <tider@quansitech.com>
2020-03-19 10:19:31 +0800

    修复迁移执行失败

--- src/Bootstrap/Context.php
+++ src/Bootstrap/Context.php
@@ -5,14 +5,14 @@ class Context{
 
     const PACKAGE_CACHE_FILE = 'qscmf-packages.php';
 
-    static public function providerRegister(){
+    static public function providerRegister($is_lara = false){
         $cache_file = LARA_DIR . '/bootstrap/cache/' . self::PACKAGE_CACHE_FILE;
         if(file_exists($cache_file)){
             $packages = require $cache_file;
-            collect($packages)->values()->each(function($item, $key){
-                collect($item['providers'])->each(function($cls, $index){
+            collect($packages)->values()->each(function($item, $key) use ($is_lara){
+                collect($item['providers'])->each(function($cls, $index) use ($is_lara){
                     if(class_exists($cls)){
-                        (new $cls())->register();
+                        $is_lara ? (new $cls())->registerLara() : (new $cls())->register();
                     }
                 });
             });
