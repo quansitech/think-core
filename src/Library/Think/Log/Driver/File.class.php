a1943dd97fd5c3929716b6a359b3857075d117b7
tiderjian <tider@quansitech.com>
2020-03-24 16:36:15 +0800

    Log类增加$_SERVER值判断，使CLI模式不再报警告信息

--- src/Library/Think/Log/Driver/File.class.php
+++ src/Library/Think/Log/Driver/File.class.php
@@ -43,6 +43,9 @@ class File {
         //检测日志文件大小，超过配置大小则备份日志文件重新生成
         if(is_file($destination) && floor($this->config['log_file_size']) <= filesize($destination) )
               rename($destination,dirname($destination).'/'.time().'-'.basename($destination));
-        error_log("[{$now}] ".$_SERVER['REMOTE_ADDR'].' '.$_SERVER['REQUEST_URI']."\r\n{$log}\r\n", 3,$destination);
+
+        $remote_addr = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+        $request_uri = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : '';
+        error_log("[{$now}] ".$remote_addr.' '.$request_uri."\r\n{$log}\r\n", 3,$destination);
     }
 }
