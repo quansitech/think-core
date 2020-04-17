63d27033c5bad8776fb8719e351f59c1fef7535f
tiderjian <tider@quansitech.com>
2020-04-17 14:13:06 +0800

    model增加generate方法，提供低内存消耗的迭代方法，原理使用php yield协程

---
+++ src/Library/Qscmf/Core/ModelHelper.class.php
@@ -0,0 +1,19 @@
+<?php
+namespace Qscmf\Core;
+
+trait ModelHelper{
+
+    public function generator($map = [], $count = 1){
+        if(!empty($map)){
+            $this->where($map);
+        }
+
+        $page = 1;
+        while($ents = $this->page($page, $count)->select()){
+            foreach($ents as $ent){
+                yield $ent;
+            }
+            $page++;
+        }
+    }
+}
