1b9c947c3ed637cb318c94542bf1a388b2f1b3bd
tiderjian <tider@quansitech.com>
2020-07-17 09:52:26 +0800

    1.ueditor可重置js config配置文件 2.ue 可自定义色板 3.ue 色板可自行输入16进制色值

--- src/Library/Qscmf/Builder/FormType/Ueditor/Ueditor.class.php
+++ src/Library/Qscmf/Builder/FormType/Ueditor/Ueditor.class.php
@@ -11,7 +11,15 @@ class Ueditor implements FormType {
         $view = new View();
         $view->assign('form', $form_type);
         $view->assign('gid', Str::uuid());
+        if(C('CUSTOM_UEDITOR_JS_CONFIG')){
+            $view->assign('configJs', C('CUSTOM_UEDITOR_JS_CONFIG'));
+            $view->assign('home_url', __ROOT__ . '/Public/libs/ueditor/');
+        }
+        else{
+            $view->assign('configJs', __ROOT__ . '/Public/libs/ueditor/ueditor.config.js');
+        }
         $content = $view->fetch(__DIR__ . '/ueditor.html');
+        
         return $content;
     }
 }
