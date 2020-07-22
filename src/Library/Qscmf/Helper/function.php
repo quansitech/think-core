eadbe5f2e6e26c713e790acc35602b0ef4c21e82
tiderjian <tider@quansitech.com>
2020-07-22 13:41:33 +0800

    优化uniquePageData

--- src/Library/Qscmf/Helper/function.php
+++ src/Library/Qscmf/Helper/function.php
@@ -4,7 +4,12 @@ if(!function_exists('uniquePageData')){
         $current_cache_key = $cache_key . '_' . $page;
         $pre_cache_key = $cache_key . '_' . ($page -1);
         if($page == 1 || !session($pre_cache_key)){
-            session($current_cache_key, $data);
+            $key_data = collect($data)->map(function($item) use ($unique_key){
+                return [
+                    $unique_key => $item[$unique_key]
+                ];
+            })->all();
+            session($current_cache_key, $key_data);
             return $data;
         }
 
