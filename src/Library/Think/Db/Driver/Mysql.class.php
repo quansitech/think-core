a3fffe13efb98adfa006795e9cfaa3fa8dc07566
tiderjian <tider@quansitech.com>
2020-09-04 10:19:04 +0800

    修补where查询可通过key值进行sql注入的漏洞

--- src/Library/Think/Db/Driver/Mysql.class.php
+++ src/Library/Think/Db/Driver/Mysql.class.php
@@ -113,6 +113,18 @@ class Mysql extends Driver{
         return $key;
     }
 
+    protected function parseColumn(&$column){
+        $column   =  trim($column);
+        if(strpos($column, '.')){
+            list($alias, $column) = explode('.', $column, 2);
+            $column = '`' . $alias . '`.`' . $column . '`';
+        }
+        else{
+            $column = '`' . $column . '`';
+        }
+        return $column;
+    }
+
     /**
      * 批量插入记录
      * @access public
