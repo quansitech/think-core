acb7e7a05995d930c8996201a24b991c9645a340
xhiny <axhiny@163.com>
2021-03-24 10:02:05 +0800

    ColumnType的Date与Time组件支持通过value自定义日期格式

--- src/Library/Qscmf/Builder/ColumnType/Date/Date.class.php
+++ src/Library/Qscmf/Builder/ColumnType/Date/Date.class.php
@@ -6,6 +6,6 @@ use Qscmf\Builder\ColumnType\ColumnType;
 class Date extends ColumnType {
 
     public function build(array &$option, array $data, $listBuilder){
-        return time_format($data[$option['name']], 'Y-m-d');
+        return time_format($data[$option['name']], $option['value'] ?:'Y-m-d');
     }
 }
