37a6e7f047831ff05d062f7efd217b8d777ed78d
xhiny <axhiny@163.com>
2021-06-11 09:40:20 +0800

    修复队列使用C函数无法获取最新qs_config配置值bug

--- src/Library/Behavior/LoadDBConfigBehavior.class.php
+++ src/Library/Behavior/LoadDBConfigBehavior.class.php
@@ -7,6 +7,9 @@ class LoadDBConfigBehavior{
      //行为执行入口
     public function run(&$param){
         try{
+            \Qscmf\Lib\Tp3Resque\Resque\Event::listen('beforePerform', function($args){
+                readerSiteConfig();
+            });
             readerSiteConfig();
         }
     	catch(\Exception $ex){
