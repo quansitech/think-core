83054ee3e0bf01814d73e6ac4f2f9e61a2d90692
viki <719632790@qq.com>
2021-08-18 14:20:12 +0800

    微信富文本

--- asset/libs/ueditor/ueditor.all.js
+++ asset/libs/ueditor/ueditor.all.js
@@ -7,113 +7,6 @@
 (function(){
 
 
-    function filter(div) {
-        var me = this;
-        var html;
-        if (div.firstChild) {
-            //去掉cut中添加的边界值
-            var nodes = domUtils.getElementsByTagName(div, 'span');
-            for (var i = 0, ni; ni = nodes[i++];) {
-                if (ni.id == '_baidu_cut_start' || ni.id == '_baidu_cut_end') {
-                    domUtils.remove(ni);
-                }
-            }
-
-            if (browser.webkit) {
-
-                var brs = div.querySelectorAll('div br');
-                for (var i = 0, bi; bi = brs[i++];) {
-                    var pN = bi.parentNode;
-                    if (pN.tagName == 'DIV' && pN.childNodes.length == 1) {
-                        pN.innerHTML = '<p><br/></p>';
-                        domUtils.remove(pN);
-                    }
-                }
-                var divs = div.querySelectorAll('#baidu_pastebin');
-                for (var i = 0, di; di = divs[i++];) {
-                    var tmpP = me.document.createElement('p');
-                    di.parentNode.insertBefore(tmpP, di);
-                    while (di.firstChild) {
-                        tmpP.appendChild(di.firstChild);
-                    }
-                    domUtils.remove(di);
-                }
-
-                var metas = div.querySelectorAll('meta');
-                for (var i = 0, ci; ci = metas[i++];) {
-                    domUtils.remove(ci);
-                }
-
-                var brs = div.querySelectorAll('br');
-                for (i = 0; ci = brs[i++];) {
-                    if (/^apple-/i.test(ci.className)) {
-                        domUtils.remove(ci);
-                    }
-                }
-            }
-            if (browser.gecko) {
-                var dirtyNodes = div.querySelectorAll('[_moz_dirty]');
-                for (i = 0; ci = dirtyNodes[i++];) {
-                    ci.removeAttribute('_moz_dirty');
-                }
-            }
-            if (!browser.ie) {
-                var spans = div.querySelectorAll('span.Apple-style-span');
-                for (var i = 0, ci; ci = spans[i++];) {
-                    domUtils.remove(ci, true);
-                }
-            }
-
-            //ie下使用innerHTML会产生多余的\r\n字符，也会产生&nbsp;这里过滤掉
-            html = div.innerHTML;//.replace(/>(?:(\s|&nbsp;)*?)</g,'><');
-
-            //过滤word粘贴过来的冗余属性
-            html = UE.filterWord(html);
-            //取消了忽略空白的第二个参数，粘贴过来的有些是有空白的，会被套上相关的标签
-            var root = UE.htmlparser(html);
-
-            //如果给了过滤规则就先进行过滤
-            if (me.options.filterRules) {
-                UE.filterNode(root, me.options.filterRules);
-            }
-            //执行默认的处理
-            me.filterInputRule(root);
-            //针对chrome的处理
-            if (browser.webkit) {
-                var br = root.lastChild();
-                if (br && br.type == 'element' && br.tagName == 'br') {
-                    root.removeChild(br)
-                }
-                utils.each(me.body.querySelectorAll('div'), function (node) {
-                    if (domUtils.isEmptyBlock(node)) {
-                        domUtils.remove(node,true)
-                    }
-                })
-            }
-            html = {'html': root.toHtml()};
-            me.fireEvent('beforepaste', html, root);
-            //抢了默认的粘贴，那后边的内容就不执行了，比如表格粘贴
-            if(!html.html){
-                return;
-            }
-            root = UE.htmlparser(html.html,true);
-            //如果开启了纯文本模式
-            if (me.queryCommandState('pasteplain') === 1) {
-                me.execCommand('insertHtml', UE.filterNode(root, me.options.filterTxtRules).toHtml(), true);
-            } else {
-                //文本模式
-                UE.filterNode(root, me.options.filterTxtRules);
-                txtContent = root.toHtml();
-                //完全模式
-                htmlContent = html.html;
-
-                address = me.selection.getRange().createAddress(true);
-                me.execCommand('insertHtml', me.getOpt('retainOnlyLabelPasted') === true ?  getPureHtml(htmlContent) : htmlContent, true);
-            }
-
-            me.fireEvent("afterpaste", html);
-        }
-    }
 // editor.js
 UEDITOR_CONFIG = window.UEDITOR_CONFIG || {};
 
@@ -11034,31 +10927,6 @@ UE.plugin.register('background', function () {
 
 
 
-// plugins/insert_richtext.js
-    /**
-     * 抓取微信富文本
-     */
-    UE.plugin.register('insert_richtext', function () {
-        var me = this;
-
-        return {
-            bindEvents: {
-                'afterInsertRichText': function (e, html) {
-                    me.execCommand('cleardoc');
-                    filter.call(me, html);
-                    me.document.body.innerHTML = html;
-
-                    me.fireEvent('catchremoteimage');
-
-                    $('.submit').trigger('startHandlePostData', '正在抓取图片');
-                },
-            },
-            commands: {
-            }
-        }
-    });
-
-
 // plugins/image.js
 /**
  * 图片插入、排版插件
@@ -14639,6 +14507,114 @@ UE.plugin.register('copy', function () {
  * @author zhanyi
  */
 UE.plugins['paste'] = function () {
+    function filter(div) {
+        var me = this;
+        var html;
+        if (div.firstChild) {
+            //去掉cut中添加的边界值
+            var nodes = domUtils.getElementsByTagName(div, 'span');
+            for (var i = 0, ni; ni = nodes[i++];) {
+                if (ni.id == '_baidu_cut_start' || ni.id == '_baidu_cut_end') {
+                    domUtils.remove(ni);
+                }
+            }
+
+            if (browser.webkit) {
+
+                var brs = div.querySelectorAll('div br');
+                for (var i = 0, bi; bi = brs[i++];) {
+                    var pN = bi.parentNode;
+                    if (pN.tagName == 'DIV' && pN.childNodes.length == 1) {
+                        pN.innerHTML = '<p><br/></p>';
+                        domUtils.remove(pN);
+                    }
+                }
+                var divs = div.querySelectorAll('#baidu_pastebin');
+                for (var i = 0, di; di = divs[i++];) {
+                    var tmpP = me.document.createElement('p');
+                    di.parentNode.insertBefore(tmpP, di);
+                    while (di.firstChild) {
+                        tmpP.appendChild(di.firstChild);
+                    }
+                    domUtils.remove(di);
+                }
+
+                var metas = div.querySelectorAll('meta');
+                for (var i = 0, ci; ci = metas[i++];) {
+                    domUtils.remove(ci);
+                }
+
+                var brs = div.querySelectorAll('br');
+                for (i = 0; ci = brs[i++];) {
+                    if (/^apple-/i.test(ci.className)) {
+                        domUtils.remove(ci);
+                    }
+                }
+            }
+            if (browser.gecko) {
+                var dirtyNodes = div.querySelectorAll('[_moz_dirty]');
+                for (i = 0; ci = dirtyNodes[i++];) {
+                    ci.removeAttribute('_moz_dirty');
+                }
+            }
+            if (!browser.ie) {
+                var spans = div.querySelectorAll('span.Apple-style-span');
+                for (var i = 0, ci; ci = spans[i++];) {
+                    domUtils.remove(ci, true);
+                }
+            }
+
+            //ie下使用innerHTML会产生多余的\r\n字符，也会产生&nbsp;这里过滤掉
+            html = div.innerHTML;//.replace(/>(?:(\s|&nbsp;)*?)</g,'><');
+
+            //过滤word粘贴过来的冗余属性
+            html = UE.filterWord(html);
+            //取消了忽略空白的第二个参数，粘贴过来的有些是有空白的，会被套上相关的标签
+            var root = UE.htmlparser(html);
+
+            //如果给了过滤规则就先进行过滤
+            if (me.options.filterRules) {
+                UE.filterNode(root, me.options.filterRules);
+            }
+            //执行默认的处理
+            me.filterInputRule(root);
+            //针对chrome的处理
+            if (browser.webkit) {
+                var br = root.lastChild();
+                if (br && br.type == 'element' && br.tagName == 'br') {
+                    root.removeChild(br)
+                }
+                utils.each(me.body.querySelectorAll('div'), function (node) {
+                    if (domUtils.isEmptyBlock(node)) {
+                        domUtils.remove(node,true)
+                    }
+                })
+            }
+            html = {'html': root.toHtml()};
+            me.fireEvent('beforepaste', html, root);
+            //抢了默认的粘贴，那后边的内容就不执行了，比如表格粘贴
+            if(!html.html){
+                return;
+            }
+            root = UE.htmlparser(html.html,true);
+            //如果开启了纯文本模式
+            if (me.queryCommandState('pasteplain') === 1) {
+                me.execCommand('insertHtml', UE.filterNode(root, me.options.filterTxtRules).toHtml(), true);
+            } else {
+                //文本模式
+                UE.filterNode(root, me.options.filterTxtRules);
+                txtContent = root.toHtml();
+                //完全模式
+                htmlContent = html.html;
+
+                address = me.selection.getRange().createAddress(true);
+                me.execCommand('insertHtml', me.getOpt('retainOnlyLabelPasted') === true ?  getPureHtml(htmlContent) : htmlContent, true);
+            }
+
+            me.fireEvent("afterpaste", html);
+        }
+    }
+
     function getClipboardData(callback) {
         var doc = this.document;
         if (doc.getElementById('baidu_pastebin')) {
