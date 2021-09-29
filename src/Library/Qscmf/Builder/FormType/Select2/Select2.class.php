70d8ee9c4159985e82a5455d10eaef5065e23c7e
xhiny <axhiny@163.com>
2021-09-29 14:09:24 +0800

    修复FormBuilder使用同字段名的select2、select组件，select组件会渲染为select2的bug。

--- src/Library/Qscmf/Builder/FormType/Select2/Select2.class.php
+++ src/Library/Qscmf/Builder/FormType/Select2/Select2.class.php
@@ -8,6 +8,7 @@ class Select2 implements FormType {
 
     public function build(array $form_type){
         $view = new View();
+        $view->assign('gid', \Illuminate\Support\Str::uuid()->toString());
         $view->assign('form', $form_type);
         if($form_type['item_option']['read_only']){
             $content = $view->fetch(__DIR__ . '/select2_read_only.html');
