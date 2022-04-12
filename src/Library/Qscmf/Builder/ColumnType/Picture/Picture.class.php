4ee19404ba008e7b39aa46c845c157c8520af21e
HH <1041729157@qq.com>
2022-04-12 16:56:46 +0800

    picture组件扩展

--- src/Library/Qscmf/Builder/ColumnType/Picture/Picture.class.php
+++ src/Library/Qscmf/Builder/ColumnType/Picture/Picture.class.php
@@ -2,10 +2,21 @@
 namespace Qscmf\Builder\ColumnType\Picture;
 
 use Qscmf\Builder\ColumnType\ColumnType;
+use Think\View;
 
 class Picture extends ColumnType {
 
     public function build(array &$option, array $data, $listBuilder){
-        return '<img src="'.showFileUrl($data[$option['name']]).'">';
+        // 如果是数组，则使用viewerjs（默认情况下不使用）
+        if (is_array($data[$option['name']])){
+            $image['original'] = showFileUrl($data[$option['name']]['original']); // 原图
+            $image['src'] = showFileUrl($data[$option['name']]['src']); // 缩略图
+        }else{
+            return '<img src="'.showFileUrl($data[$option['name']]).'">';
+        }
+        $view = new View();
+        $view->assign('image', $image);
+        $content = $view->fetch(__DIR__ . '/pictures.html');
+        return $content;
     }
 }
