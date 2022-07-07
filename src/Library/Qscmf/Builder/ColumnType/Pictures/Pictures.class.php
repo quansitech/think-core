b060707741d61c49f24cf4e7cbe236fdd843d2a3
HH <1041729157@qq.com>
2022-07-07 16:30:21 +0800

    pictures组件扩展

---
+++ src/Library/Qscmf/Builder/ColumnType/Pictures/Pictures.class.php
@@ -0,0 +1,32 @@
+<?php
+namespace Qscmf\Builder\ColumnType\Pictures;
+
+use Qscmf\Builder\ColumnType\ColumnType;
+use Think\View;
+
+class Pictures extends ColumnType
+{
+    public function build(array &$option, array $data, $listBuilder)
+    {
+        $pic = explode(',', $data[$option['name']]);
+        $images = [];
+        foreach($pic as $v){
+            switch ($option['value']){
+                case 'oss':
+                    $small_url = showFileUrl($v).'?x-oss-process=image/resize,m_fill,w_40,h_40';
+                    break;
+                default:
+                    $small_url = \Qscmf\Utils\Libs\Common::imageproxy('40x40', $v);
+                    break;
+            }
+            $images[] = [
+                'url' => showFileUrl($v),
+                'small_url' => $small_url,
+            ];
+        }
+        $view = new View();
+        $view->assign('images', $images);
+        $content = $view->fetch(__DIR__ . '/pictures.html');
+        return $content;
+    }
+}
