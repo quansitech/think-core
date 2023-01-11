c16b400a185662d97cc87ab7e231ce2d17ae68b8
tiderjian <tider@quansitech.com>
2023-01-11 17:40:02 +0800

    listbuilder picture组件增加图片预览功能

--- src/Library/Qscmf/Builder/ColumnType/Picture/Picture.class.php
+++ src/Library/Qscmf/Builder/ColumnType/Picture/Picture.class.php
@@ -2,10 +2,23 @@
 namespace Qscmf\Builder\ColumnType\Picture;
 
 use Qscmf\Builder\ColumnType\ColumnType;
+use Think\View;
 
 class Picture extends ColumnType {
 
     public function build(array &$option, array $data, $listBuilder){
-        return '<img src="'.showFileUrl($data[$option['name']]).'">';
+        $view = new View();
+        $image = [
+            'url' => showFileUrl($data[$option['name']]),
+        ];
+        if(isset($option['value']) && $option['value']['small-url'] instanceof \Closure){
+            $image['small_url'] = call_user_func($option['value']['small-url'], $data[$option['name']]);
+        }
+        else{
+            $image['small_url'] = $image['url'];
+        }
+        $view->assign('image', $image);
+        $content = $view->fetch(__DIR__ . '/picture.html');
+        return $content;
     }
 }
