e7d1ac8114a6a586fc4daac0b002cdabb9832308
Jaya <frowa@foxmail.com>
2023-03-07 10:39:55 +0800

    Update Self_.class.php
    完善self功能

--- src/Library/Qscmf/Builder/ListSearchType/Self/Self_.class.php
+++ src/Library/Qscmf/Builder/ListSearchType/Self/Self_.class.php
@@ -6,6 +6,11 @@ use Qscmf\Builder\ListSearchType\ListSearchType;
 class Self_ implements ListSearchType{
 
     public function build(array $item){
-        return $item['options']['value']?:'';
+        $templateFile = $item['options']['templateFile']?:'';
+        $content= $item['options']['content']?:'';
+
+        $view = new View();
+        $view->assign('item', $item);
+        return empty($templateFile)? $content : $view->fetch($templateFile,$content);
     }
 }
