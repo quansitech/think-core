136c1e33a7f2de1f6faf7f3b6e582b9cc06066b1
W-CLL <2420583777@qq.com>
2023-03-23 14:05:55 +0800

    SubTableBuilder添加datetime字段

---
+++ src/Library/Qscmf/Builder/ColumnType/Datetime/Datetime.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Qscmf\Builder\ColumnType\Datetime;
+
+use Illuminate\Support\Str;
+use Qscmf\Builder\ColumnType\ColumnType;
+use Qscmf\Builder\ColumnType\Date\Date;
+use Qscmf\Builder\ColumnType\EditableInterface;
+
+class Datetime extends Date {
+    protected string $_temple =  __DIR__ . '/datetime.html';
+    protected string $_default_format =  'Y-m-d h:i:s';
+}
