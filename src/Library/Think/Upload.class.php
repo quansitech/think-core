4d07a043ec1ed97f98b0d7e24c4dea755438ad19
几页 <frowa@foxmail.com>
2023-11-02 11:02:08 +0800

    修改upload上传文件大小不符的提示内容，加上大小提示。

--- src/Library/Think/Upload.class.php
+++ src/Library/Think/Upload.class.php
@@ -286,7 +286,7 @@ class Upload {
 
         /* 检查文件大小 */
         if (!$this->checkSize($file['size'])) {
-            $this->error = '上传文件大小不符！'.'(<='.floor($file['size']/1024/1024).'MB)';
+            $this->error = '上传文件大小不符！'.'(<='.floor($this->config['maxSize']/1024/1024).'MB)';
             return false;
         }
 
