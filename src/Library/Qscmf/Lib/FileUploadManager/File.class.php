0029607ad8e5fe81a2b147b39861534a2b0cf9a2
tiderjian <tider@quansitech.com>
2023-11-24 17:15:13 +0800

    think-core内置上传接口，并且带hash去重功能

---
+++ src/Library/Qscmf/Lib/FileUploadManager/File.class.php
@@ -0,0 +1,33 @@
+<?php
+namespace Qscmf\Lib\FileUploadManager;
+
+class File{
+    public string $title;
+    public string $file;
+    public string $url;
+    public int $size;
+    public string $cate;
+    public string $mime_type;
+    public int $security;
+    public int $owner;
+    public int $upload_date;
+    public string $hash_id;
+
+    private array $meta = [
+        "file", "url", "size", "hash_id", "mime_type"
+    ];
+
+    public function __construct($fileInfo = []) {
+        foreach ($fileInfo as $key => $value) {
+            if(property_exists($this, $key)) {
+                $this->{$key} = $value;
+            }
+        }
+    }
+
+    public function merge(File $source_file){
+        foreach($this->meta as $key){
+            $this->{$key} = $source_file->{$key};
+        }
+    }
+}
