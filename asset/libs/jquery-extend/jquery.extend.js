8ac95531b7397a899ab6cd51e66afd4567c809b4
lailixiaoheihong1 <1591486641@qq.com>
2023-11-29 14:16:01 +0800

    删除原来的，修改cui.extend.js中废弃的语法

--- asset/libs/jquery-extend/jquery.extend.js
+++ asset/libs/jquery-extend/jquery.extend.js
@@ -1,14 +1,6 @@
 // 自己手动扩展的jQuery方法
 (function($) {
-    $.extend({
-        isWindow: function( obj ) {
-            return obj != null && obj === obj.window;
-        },
-        isArray: function(obj) {
-            return Array.isArray(obj);
-        },
-        isFunction: function(obj) {
-            return typeof obj === "function" && obj instanceof Function;
-        },
-    })
+    $.fn.size = function() {
+        return this.length;
+    }
 })(jQuery);
