7d80d617db77fa495a12c300885a4c4edad940df
tiderjian <tider@quansitech.com>
2024-04-18 10:05:18 +0800

    DateRange ListSearchType 增加 parseForTimestamp 方法，处理数据库格式为timestamp的情形

--- src/Library/Qscmf/Builder/ListSearchType/DateRange/DateRange.class.php
+++ src/Library/Qscmf/Builder/ListSearchType/DateRange/DateRange.class.php
@@ -17,10 +17,29 @@ class DateRange implements ListSearchType{
 
     //样例代码 $map = array_merge($map, DateRange::parse('date_range_data', 'create_date', $get_data));
     static public function parse(string $key, string $map_key, array $get_data) : array{
+        return self::parseForDecimal($key, $map_key, $get_data);
+    }
+
+
+    //数据库格式为decimal
+    static public function parseForDecimal(string $key, string $map_key, array $get_data) : array{
         if(isset($get_data[$key])){
             $date_range = explode('-', $get_data[$key]);
             $start_time = strtotime(trim($date_range[0]));
             $end_time = strtotime(trim($date_range[1]).'+1 day') -1;
+            return [$map_key => ['BETWEEN', [$start_time, $end_time]]];
+        }
+        else{
+            return [];
+        }
+    }
+
+    //数据库格式为timestamp
+    static public function parseForTimestamp(string $key, string $map_key, array $get_data) : array{
+        if(isset($get_data[$key])){
+            $date_range = explode('-', $get_data[$key]);
+            $start_time = trim($date_range[0]);
+            $end_time = trim($date_range[1]) . " 23:59:59.9999";
             return [$map_key => ['BETWEEN', [$start_time, $end_time]]];
         }
         else{
