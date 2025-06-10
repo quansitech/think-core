bb01bd49581a6018fcdd83939e4abf2e05b6ca9b
tider <tider@quansitech.com>
2025-06-10 14:58:35 +0800

    form_type date num radio增加只读功能

--- src/Library/Qscmf/Builder/FormType/Num/Num.class.php
+++ src/Library/Qscmf/Builder/FormType/Num/Num.class.php
@@ -10,7 +10,13 @@ class Num implements FormType {
     public function build(array $form_type){
         $view = new View();
         $view->assign('form', $form_type);
-        $content = $view->fetch(__DIR__ . '/num.html');
+        
+        if($form_type['item_option']['read_only']){
+            $content = $view->fetch(__DIR__ . '/num_read_only.html');
+        }
+        else{
+            $content = $view->fetch(__DIR__ . '/num.html');
+        }
         return $content;
     }
 }
