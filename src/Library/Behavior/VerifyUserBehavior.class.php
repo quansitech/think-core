69e56a450dc30bf547d633ed1b4e5ee4498787a8
tider <tider@quansitech.com>
2026-01-12 11:59:52 +0800

    t

--- src/Library/Behavior/VerifyUserBehavior.class.php
+++ src/Library/Behavior/VerifyUserBehavior.class.php
@@ -3,14 +3,15 @@
 namespace Behavior;
 
 use Gy_Library\DBCont;
+use Illuminate\Database\Capsule;
 
 class VerifyUserBehavior
 {
 
     public function run(){
         //非正常状态用户禁止登录后台
-        $user_ent = D(C('USER_AUTH_MODEL'))->find(session(C('USER_AUTH_KEY')));
-        if((int)$user_ent['status'] !== DBCont::NORMAL_STATUS){
+        $user_ent = Capsule::table(C('USER_AUTH_MODEL'))->find(session(C('USER_AUTH_KEY')));
+        if((int)$user_ent->status !== DBCont::NORMAL_STATUS){
             E('用户状态异常');
         }
     }
